e0ef14f4d3856fcb0baa202a586b622e1f525189
Jason Telfer <jason@jasont.dev>
2022-08-05 21:54:31 +1000

    Tasklist creation

--- src/routes/CreateTasklist.tsx
+++ src/routes/CreateTasklist.tsx
@@ -4,29 +4,45 @@ import {
   UnauthenticatedTemplate,
   useMsal,
 } from "@azure/msal-react";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
-import { NewTasklist } from "../model";
-import { getUser } from "../queries";
+import { NewTasklist, Tasklist, User } from "../model";
+import { useForm } from "react-hook-form";
+import {Navigate, useNavigate} from "react-router-dom";
 
 function CreateTasklist() {
   const { instance, accounts } = useMsal();
   const [accessToken, setAccessToken] = useState("");
+  const { register, handleSubmit } = useForm<NewTasklist>();
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
 
-  const { data } = useQuery(["tasklists"], {
-    enabled: accessToken !== "",
-    queryFn: () => {
-      return getUser(accounts[0].localAccountId, accessToken);
+  const newTasklistMutation = useMutation(
+    (newTasklist: NewTasklist) => {
+      return axios
+        .post("http://localhost:8080/tasklist", newTasklist, {
+          headers: { Authorization: `Bearer ${accessToken}` },
+        })
+        .then((response) => response.data);
     },
-  });
-
-  const newTasklistMutation = useMutation((newTasklist: NewTasklist) => {
-    return axios
-      .post("http://localhost:8080/tasklist", newTasklist, {
-        headers: { Authorization: `Bearer ${accessToken}` },
-      })
-      .then((response) => response.data);
-  });
+    {
+      onSuccess: (data) => {
+        // The goal here is to assimilate the tasklist with the old user data before
+        // navigating to the new tasklist page
+        const oldQueryData = queryClient.getQueryData<User>([ "tasklists" ]);
+        if(oldQueryData) {
+          if(oldQueryData.tasklists) {
+            oldQueryData.tasklists.push(data);
+          } else {
+            oldQueryData.tasklists = [data];
+          }
+        }
+        // TODO: under what circumstances might there be no query data already?
+        queryClient.setQueryData(["tasklists"], oldQueryData);
+        navigate(`/tasklist/${data.id}`)
+      },
+    }
+  );
 
   useEffect(() => {
     instance
@@ -40,9 +56,25 @@ function CreateTasklist() {
   }, []);
 
   return (
-    <div>
+    <div className="p-8">
       <AuthenticatedTemplate>
-        {data && data.tasklists ? <div></div> : null}
+        <form
+          onSubmit={handleSubmit((data) => {
+            let newTasklist = { ...data };
+            newTasklist.users = [];
+            newTasklist.users.push({ id: accounts[0].localAccountId });
+            newTasklistMutation.mutate(newTasklist);
+          })}
+        >
+          <input
+            {...register("name")}
+            className="text-3xl font-bold"
+            placeholder="Title"
+          ></input>
+          <button type="submit" className="bg-blue-400">
+            Save
+          </button>
+        </form>
       </AuthenticatedTemplate>
       <UnauthenticatedTemplate></UnauthenticatedTemplate>
     </div>
